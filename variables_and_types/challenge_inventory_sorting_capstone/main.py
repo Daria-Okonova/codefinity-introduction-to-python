bfe5018c79edc4aff76ae18bc600fd3f9c3f5669
Daria-Okonova <>
2026-01-12 08:57:10 +0000

    Challenge: Inventory Sorting Capstone

--- variables_and_types/challenge_inventory_sorting_capstone/main.py
+++ variables_and_types/challenge_inventory_sorting_capstone/main.py
@@ -6,19 +6,18 @@ b_price = "$1.50"
 c_price = "$2.00"
 p_price = "$5.40"
 
+i = items.split(", ")
+c = categories.split(", ")
 
-itemsspl = items.split(", ")
-categoriesspl = categories.split(", ")
+candy1 = i[0]
+candy2 = i[1]
+dry_goods = i[2]
 
+cat1 = c[0]
+cat2 = c[1]
 
-candy1 = itemsspl[0]
-candy2 = itemsspl[1]
-dry_goods = itemsspl[2]
-
-cat1 = categoriesspl[0]
-cat2 = categoriesspl[1]
-
-
-print (f"we have {candy1} for {b_price}" )
+print(f"we have {candy1} for {b_price} in the {cat1}")
 	
-print ()
+print(f"we have {candy2} for {c_price} in the {cat1}")
+	
+print(f"we have {dry_goods} for {p_price} in the {cat2}")
